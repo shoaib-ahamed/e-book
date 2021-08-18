76efe45d5d70071c6b304282cfc0db1a7607204e
shoaib ahamed <shoaibahamedshafi@gmail.com>
2021-08-18 16:49:53 +0600

    few changes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   env: {
     "BASE_URL" : "https://e-book-mocha.vercel.app/",
-    "MONGODB_URL" : "mongodb+srv://shoaibahamed:shoaibahamed@cluster0.go4eh.mongodb.net/e-book?retryWrites=true&w=majority",
-    "ACCESS_TOKEN_SECRET" : "shoaibahamedshafi@gmail.com01672019292",
-    "REFRESH_TOKEN_SECRET" : "shoaibahamedshafi@gmail.com0167201929201303082197",
+    "MONGODB_URL" : "mongodb+srv://shnova0987654321:shnova0987654321@cluster0.go4eh.mongodb.net/e-book?retryWrites=true&w=majority",
+    "ACCESS_TOKEN_SECRET" : "shnova0987654321@gmail.com01672019292123412",
+    "REFRESH_TOKEN_SECRET" : "shnova0987654321@gmail.com0167201929201303082197",
     "CLOUD_UPDATE_PRESET": "nextjs_store",
     "CLOUD_NAME": "shoaibahamed",
     "CLOUD_API": "https://api.cloudinary.com/v1_1/shoaibahamed/image/upload"
